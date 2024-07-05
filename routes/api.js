73e0f3231ac528559f6667b33e5a6b0640cdc06e
David ADE <david.ade.1989@gmail.com>
2024-07-05 16:39:49 +0200

    first try with project

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,50 @@
 
 module.exports = function (app) {
 
+const stockAPIUrl = 'https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/[symbol]/quote'
+
+
   app.route('/api/stock-prices')
-    .get(function (req, res){
+    .get(async (req, res) => {
+      // retrieve stock and like from query
+      let stock = req.query.stock
+      let like = req.query.like
       
+      let result = []
+      
+      try {
+        if (typeof(stock) === 'string') {
+          let response = await fetch(stockAPIUrl.replace('[symbol]', stock))
+          const data = await response.json()
+          result = {
+            stock: data.symbol,
+            price: data.latestPrice,
+            likes: data.likes
+          }
+
+        } else if (typeof(stock) === 'object') {
+          for (let i=0; i<stock.length; i++) {
+            let response = await fetch(stockAPIUrl.replace('[symbol]', stock[i]))
+            const data = await response.json()
+            
+            result.push({
+              stock: data.symbol,
+              price: data.latestPrice,
+              likes: data.likes
+            })
+          }
+        }
+        
+        
+        res.json({
+          stockData: result
+        })
+    
+        
+      } 
+      catch (error) {
+        console.error(error)
+      }
     });
     
 };
